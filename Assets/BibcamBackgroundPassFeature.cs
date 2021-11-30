f2827e6ff3f382c2876ef8c2e09f0209f53341b6
Keijiro Takahashi <keijiro@gmail.com>
2021-11-30 12:03:08 +0900

    Sync with Bibcam package

--- Assets/BibcamBackgroundPassFeature.cs
+++ Assets/BibcamBackgroundPassFeature.cs
@@ -1,38 +1,24 @@
-using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
-using Bibcam.Common;
-using Bibcam.Decoder;
 
-namespace Bibcam {
+namespace Bibcam.Decoder {
 
-sealed class BackgroundRenderPass : ScriptableRenderPass
+sealed class BibcamBackgroundRenderPass : ScriptableRenderPass
 {
-    public BibcamMetadataDecoder Decoder { get; set; }
-    public BibcamTextureDemuxer Demuxer { get; set; }
-    public Material Material { get; set; }
-
     public override void Execute
       (ScriptableRenderContext context, ref RenderingData renderingData)
     {
-        if (Decoder == null || Demuxer == null) return;
-        if (Demuxer.ColorTexture == null) return;
+        // Play mode is not yet supported.
+        if (!UnityEngine.Application.isPlaying) return;
 
-        // Camera parameters
-        var meta = Decoder.Metadata;
-        var ray = BibcamRenderUtils.RayParams(meta);
-        var iview = BibcamRenderUtils.InverseView(meta);
+        // BibcamBackground component reference
+        var camera = renderingData.cameraData.camera;
+        var bg = camera.GetComponent<BibcamBackground>();
+        if (bg == null) return;
 
-        // Material property update
-        Material.SetVector(ShaderID.RayParams, ray);
-        Material.SetMatrix(ShaderID.InverseView, iview);
-        Material.SetVector(ShaderID.DepthRange, meta.DepthRange);
-        Material.SetTexture(ShaderID.ColorTexture, Demuxer.ColorTexture);
-        Material.SetTexture(ShaderID.DepthTexture, Demuxer.DepthTexture);
-
-        // Draw call
+        // Command buffer
         var cmd = CommandBufferPool.Get();
-        CoreUtils.DrawFullScreen(cmd, Material);
+        bg.PushDrawCommand(cmd);
         context.ExecuteCommandBuffer(cmd);
         CommandBufferPool.Release(cmd);
     }
@@ -40,59 +26,15 @@ sealed class BackgroundRenderPass : ScriptableRenderPass
 
 public sealed class BibcamBackgroundPassFeature : ScriptableRendererFeature
 {
-    #region Public properties/methods
-
-    public void SetSource
-      (BibcamMetadataDecoder decoder, BibcamTextureDemuxer demuxer)
-      => (_pass.Decoder, _pass.Demuxer) = (decoder, demuxer);
-
-    public float DepthOffset
-      { set => Material.SetFloat(ShaderID.DepthOffset, value); }
-
-    public Color DepthColor
-      { set => Material.SetColor(ShaderID.DepthColor, value); }
-
-    public Color StencilColor
-      { set => Material.SetColor(ShaderID.StencilColor, value); }
-
-    #endregion
-
-    #region Project asset references
-
-    [SerializeField, HideInInspector] Shader _shader = null;
-
-    #endregion
-
-    #region Private members
-
-    Material _material;
-    BackgroundRenderPass _pass;
-
-    Material Material
-      => _material != null ? _material :
-           _material = CoreUtils.CreateEngineMaterial(_shader);
-
-    #endregion
-
-    #region ScriptableRendererFeature overrides
+    BibcamBackgroundRenderPass _pass;
 
     public override void Create()
-    {
-        _pass = new BackgroundRenderPass { Material = this.Material };
-        _pass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
-    }
+      => _pass = new BibcamBackgroundRenderPass
+           { renderPassEvent = RenderPassEvent.AfterRenderingOpaques };
 
     public override void AddRenderPasses
       (ScriptableRenderer renderer, ref RenderingData renderingData)
       => renderer.EnqueuePass(_pass);
-
-    protected override void Dispose(bool disposing)
-    {
-        CoreUtils.Destroy(_material);
-        _material = null;
-    }
-
-    #endregion
 }
 
-} // namespace Bibcam
+} // namespace Bibcam.Decoder
